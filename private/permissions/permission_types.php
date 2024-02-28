43893226d3183ab5f165667745425e67a7650a66
Nicholas Quednow <gitcommits@cloudevelop.dev>
2024-02-27 20:42:29 -0500

    add permission base

---
+++ private/permissions/permission_types.php
@@ -0,0 +1,11 @@
+<?php
+$PERMISSION_CREATE_USER = 'create_user';
+$PERMISSION_INVITE_USER = 'invite_user';
+$PERMISSION_CREATE_GROUP = 'create_group';
+$PERMISSION_LINK_USER_TO_GROUP = 'link_user_to_group';
+$permission_defaults = array(
+    'create_user'=>0,
+    'invite_user'=>0,
+    'create_group'=>0,
+    'link_user_to_group'=>0
+);
