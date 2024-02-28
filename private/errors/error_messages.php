0555bea82f8bb2f1ad6a1009ea1aea5e0b845b51
Nicholas Quednow <gitcommits@cloudevelop.dev>
2024-02-27 20:38:03 -0500

    add error code and message template files

---
+++ private/errors/error_messages.php
@@ -0,0 +1,7 @@
+<?php
+$ERROR_MESSAGES = array(
+    0001=>"A Malformed request was made, and a log has been made of this. if the error persists, please notify the administrator.",
+    0002=>"An Internal query ocurred.",
+    0003=>"You do not have permission to access this resource",
+    0004=>"The session is not valid. Please log in."
+);
